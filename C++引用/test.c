ff9da42b6ce2d3155339a523470876562601245d
hanchang12345 <120896886+hanchang12345@users.noreply.github.com>
2023-04-18 22:43:04 +0800

    这是加上的测试代码

---
+++ C++引用/test.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("hehehe\n");
+    printf("哈哈\n");
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("hello.world\n");
+    printf("哈哈哈\n");
+    return 0;
+}
